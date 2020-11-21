6241a8d5a62cd3042e9f0eeb0e179ed4dd78c35b
Suryansh Jain <54480215+iamsuryansh@users.noreply.github.com>
2020-11-22 00:36:33 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+$(".submit-icon").click(function(){
+  // var email;
+  // email = document.getElementById("myForm").value();
+  //
+  // if (email==="suryanshjain531@gmail.com") {
+  //   alert("thanks");
+  //   document.getElementById("myForm").submit();
+  // }
+  // else {
+  // $(".error-icon").classList.add("show");
+  // }
+  var element = document.getElementById("error-id");
+  element.classList.add("show");
+
+
+ })
